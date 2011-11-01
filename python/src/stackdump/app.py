dc7a923fa94e5f5db3418dc2d32c7f9828023376
Samuel Lai <samuel.lai@gmail.com>
2011-11-01 22:03:22 +1100

    Fleshed initial HTML views and pages.

--- python/src/stackdump/app.py
+++ python/src/stackdump/app.py
@@ -1,14 +1,19 @@
-from bottle import route, run, static_file
-from jinja2 import Environment, PackageLoader
-
 import sys
 import os
+
+from bottle import route, run, static_file, debug, abort, request, redirect
+from jinja2 import Environment, PackageLoader
+from sqlobject import sqlhub, connectionForURI, AND, OR, SQLObjectNotFound 
+from pysolr import Solr
+
+from stackdump.models import Site, Badge, Comment, User
 
 # STATIC VARIABLES
 BOTTLE_ROOT = os.path.abspath(os.path.dirname(sys.argv[0]))
 MEDIA_ROOT = os.path.abspath(BOTTLE_ROOT + '/../../media')
 
 # hopefully this is thread-safe; not sure though. Will need to experiment/check.
+# TODO: thread-safe?
 TEMPLATE_ENV = Environment(loader=PackageLoader('stackdump', 'templates'))
 
 # WEB REQUEST METHODS
@@ -18,13 +23,70 @@ TEMPLATE_ENV = Environment(loader=PackageLoader('stackdump', 'templates'))
 def serve_static(filename):
     return static_file(filename, root=MEDIA_ROOT)
 
-@route('/hello')
-def hello():
-    return "Hello World!"
-
 @route('/')
 def index():
-    return render_template('index.html')
+    context = { }
+    context['site_root_path'] = ''
+    context['sites'] = Site.select()
+    return render_template('index.html', context)
+
+@route('/:site_key#\w+#')
+@route('/:site_key#\w+#/')
+def site_index(site_key):
+    context = { }
+    context['site_root_path'] = '%s/' % site_key
+    
+    try:
+        context['site'] = Site.selectBy(key=site_key).getOne()
+    except SQLObjectNotFound:
+        abort(code=404, output='No site exists with the key %s.' % site_key)
+    
+    return render_template('site_index.html', context)
+
+@route('/search')
+def search():
+    query = request.GET.get('q')
+    if not query:
+        redirect(settings.APP_URL_ROOT)
+    
+    page = request.GET.get('p', 0)
+    rows_per_page = request.GET.get('r', 10)
+    
+    # perform search
+    results = solr.search(query, start=page*rows_per_page, rows=rows_per_page)
+    
+    context = { }
+    # TODO: scrub this first to avoid injection attacks?
+    context['query'] = query
+    context['results'] = results
+    
+    return render_template('results.html', context)
+
+@route('/:site_key#\w+#/search')
+def site_search(site_key):
+    context = { }
+    context['site_root_path'] = '%s/' % site_key
+    
+    try:
+        context['site'] = Site.selectBy(key=site_key).getOne()
+    except SQLObjectNotFound:
+        raise HTTPError(code=404, output='No site exists with the key %s.' % site_key)
+    
+    query = request.GET.get('q')
+    if not query:
+        redirect(settings.APP_URL_ROOT)
+    
+    page = request.GET.get('p', 0)
+    rows_per_page = request.GET.get('r', 10)
+    
+    # perform search
+    results = solr.search(query, start=page*rows_per_page, rows=rows_per_page)
+    
+    # TODO: scrub this first to avoid injection attacks?
+    context['query'] = query
+    context['results'] = results
+    
+    return render_template('site_results.html', context)
 
 # END WEB REQUEST METHODS
 
@@ -51,11 +113,27 @@ def get_template_settings():
 # INITIALISATION
 
 if __name__ == '__main__':
-    # only print this on the parent process, not the child ones. Applies when
+    # only do these things in the child processes, not the parents. Applies when
     # the auto-reload option is on (reloader=True). When it is on, the
     # BOTTLE_CHILD env var is True if this is the child process.
-    if not os.environ.get('BOTTLE_CHILD', True):
+    if os.environ.get('BOTTLE_CHILD', True):
         print('Serving media from: %s' % MEDIA_ROOT)
+        
+        # connect to the data sources
+        db_path = os.path.abspath(os.path.join(BOTTLE_ROOT, '../../../data/stackdump.sqlite'))
+    
+        # connect to the database
+        # TODO: thread-safe?
+        print('Connecting to the database...')
+        conn_str = 'sqlite://' + db_path
+        sqlhub.processConnection = connectionForURI(conn_str)
+        print('Connected.\n')
+        
+        # connect to solr
+        # TODO: thread-safe?
+        print('Connecting to solr...')
+        solr = Solr("http://localhost:8983/solr/")
+        print('Connected.\n')
     
     # load the settings file
     __import__('settings')
@@ -64,6 +142,9 @@ if __name__ == '__main__':
         settings = dict([ (k, getattr(settings, k)) for k in dir(settings) if not k.startswith('__') ])
     else:
         settings = { }
+    
+    if settings.get('DEBUG', False):
+        debug(True)
     
     # run the server!
     server = settings.get('SERVER_ADAPTER', 'wsgiref')
--- python/src/stackdump/settings.py
+++ python/src/stackdump/settings.py
@@ -3,6 +3,8 @@
 # It is modelled after the Django settings file. This file is just like any
 # other Python file, except the local variables form the settings dictionary.
 
+DEBUG = True
+
 # see http://bottlepy.org/docs/dev/tutorial.html#multi-threaded-server
 SERVER_ADAPTER = 'cherrypy'
 SERVER_HOST = '0.0.0.0'
